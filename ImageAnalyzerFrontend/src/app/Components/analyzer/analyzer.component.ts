63731e951cc1484f2154551bc8415c348b2b4cc1
PatanImranAliKhan <imranirfanalikhan786@gmail.com>
2023-08-01 17:38:02 +0530

    FE + BE complete

--- ImageAnalyzerFrontend/src/app/Components/analyzer/analyzer.component.ts
+++ ImageAnalyzerFrontend/src/app/Components/analyzer/analyzer.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 @Component({
@@ -6,15 +6,18 @@ import { HttpClient } from '@angular/common/http';
   templateUrl: './analyzer.component.html',
   styleUrls: ['./analyzer.component.css'],
 })
-export class AnalyzerComponent {
+export class AnalyzerComponent implements OnInit {
   public imageURL: any = '';
   public imageFile: any;
   public Response: any;
   public receiveResult = false;
+  public loading: boolean = false;
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) { }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.loading = false;
+  }
 
   /**
    * Function called whenever the Image file has been changes or inserted
@@ -34,31 +37,55 @@ export class AnalyzerComponent {
 
   AnalyzeImage() {
     if (this.imageURL != '') {
+      this.loading = true;
       const formData = new FormData();
       formData.append("file", this.imageFile, this.imageFile.name);
-      
+
       this.http
         .post('http://localhost:5000/upload', formData)
         .subscribe((resp: any) => {
           this.Response = JSON.parse(resp);
-          console.log(this.Response);
-          
-          if(this.Response.Status=="Error") {
-            this.receiveResult = false;
-            alert(this.Response.Message)
-            this.Response={};
-          } else if(this.Response.Status=="Prediction Error") {
-            this.receiveResult = true;
-            alert(this.Response.Message)
-          } else {
-            this.receiveResult = true;
-          }
-        },(err) => {
+          this.AnalyzeResult();
+        }, (err) => {
+          this.loading = false;
           this.receiveResult = false;
-          console.log("Error");
           this.Response = "";
-          alert("")
+          alert("Error in Processing the request")
         });
+    }
+  }
+
+  AnalyzeResult() {
+    console.log(this.Response);
+    switch (this.Response.Status) {
+      case "Error":
+        this.receiveResult = false;
+        alert(this.Response.Message)
+        break;
+      case "Prediction Error":
+        this.receiveResult = true;
+        alert(this.Response.Message);
+        break;
+      default:
+        this.receiveResult = true;
+    }
+    this.loading = false;
+    this.scrollToBottom();
+  }
+
+  scrollToBottom = () => {
+    try {
+      setTimeout(() => {
+        if (document.getElementById('result')) {
+          document.getElementById('result')?.scrollIntoView({
+            behavior: 'auto',
+            block: 'center',
+            inline: 'center',
+          });
+        }
+      }, 1000);
+    } catch (err) {
+      console.log("scrolll error: " + err)
     }
   }
 }
